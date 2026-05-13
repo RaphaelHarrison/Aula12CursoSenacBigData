a23d802d08225a5e563b1a2f0e1c1bac820b7477
RaphaelHarrison <raphaelsantos229@gmail.com>
2026-05-12 21:23:17 -0300

    Aula 01 Módulo 2

---
+++ AULA1/comandos.py
@@ -0,0 +1,7 @@
+# python -m venv venv
+# source ./venv/Scripts/activate
+# pip install pandas
+# deactivate
+
+# python -m venv venv
+# pip install -r requirements.txt
---
+++ AULA1/exemplo01.py
@@ -0,0 +1,3 @@
+import pandas as pd
+
+print("Listas do Python x Séries do Pandas")
